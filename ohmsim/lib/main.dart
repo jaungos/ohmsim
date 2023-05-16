7b74e7d84d977888da1a8586678a9908390a3d21
Junel Alje Isanan <jbisanan@up.edu.ph>
2023-05-16 21:49:13 +0800

    Reconfigure main and firebase_options

--- ohmsim/lib/main.dart
+++ ohmsim/lib/main.dart
@@ -1,15 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ohmsim/providers/authProvider.dart';
+import 'package:ohmsim/providers/adminProvider.dart';
 import 'package:ohmsim/providers/sampleprovider.dart';
+import 'package:ohmsim/screens/adminView.dart';
 import 'package:ohmsim/screens/login.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 
+late final FirebaseApp app;
+late final FirebaseAuth auth;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  app = await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  auth = FirebaseAuth.instanceFor(app: app);
 
   runApp(const MyApp());
 }
@@ -22,16 +31,19 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<ServiceProvider>(
-            create: (_) => ServiceProvider())
+            create: (_) => ServiceProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => AdminProvider())
       ],
       child: MaterialApp(
         title: 'OHMSIM',
         initialRoute: '/',
         routes: {
           //TODO: Put routes here
-          '/': (context) => LoginPage(
+          LoginPage.routeName: (context) => LoginPage(
                 key: key,
               ),
+          AdminView.routeName: (context) => AdminView()
         },
         // onGenerateRoute: (settings) {
         //   //TODO: Edit according to the planned routes
