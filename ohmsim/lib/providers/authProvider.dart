3875c5eb62a79818161ccad1826bbbf5fdb0ba5f
Junel Alje Isanan <jbisanan@up.edu.ph>
2023-05-17 14:17:29 +0800

    Reinstate simple authentication

--- ohmsim/lib/providers/authProvider.dart
+++ ohmsim/lib/providers/authProvider.dart
@@ -5,8 +5,7 @@ import 'package:ohmsim/models/samplemodel.dart';
 
 class AuthProvider with ChangeNotifier {
   String _id = "";
-  String _type = "";
-  String _signupType = "";
+  String _privilege = "";
   String _view = "";
 
   String _status = "";
@@ -14,16 +13,25 @@ class AuthProvider with ChangeNotifier {
   String _name = "";
 
   String get id => _id;
-  String get signupType => _signupType;
+
+  String get privilege => _privilege;
   String get view => _view;
   String get status => _status;
   String get name => _name;
 
   List<List<String>> data = [
-    ["Juan Makasalanan", "123456", "juanmakasalanan@up.edu.ph", "User"]
+    ["Juan Makasalanan", "123456", "juanmakasalanan@up.edu.ph", "Admin"]
   ];
 
   Future<Map> authenticateUser(email, password) {
+    for (int i = 0; i < data.length; i++) {
+      if (data[i][2] == email && data[i][1] == password) {
+        _name = data[i][0];
+        _privilege = data[i][3];
+        return Future.value(
+            {"isLoggedIn": true, "message": "Logged In!", data: data});
+      }
+    }
     return Future.value(
         {"isLoggedIn": false, "message": "User does not exist", data: []});
   }
