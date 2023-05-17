52e28b2614cdc247d61addadd5892a95e48770c1
lukepinedaa <ldpineda1@up.edu.ph>
2023-05-17 09:19:00 +0800

    Add model and provider for studentUser

---
+++ ohmsim/lib/providers/studentUser_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/studentUserModel.dart';
+
+class studentUserProvider with ChangeNotifier{
+  
+
+  void addStudentUser(studentUser user) async {
+
+  }
+  void addEntry(String entry) async {
+
+  }
+  void deleteEntry(String entry) async {
+
+  }
+}
