0fe9e5be90df0ac58b1e20b990149f9243ae57be
jlaungos <jaungos@up.edu.ph>
2023-06-02 04:54:12 +0800

    Chore: renamed file names

---
+++ ohmsim/lib/screens/userview.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:ohmsim/screens/homepage.dart';
+
+class UserView extends StatefulWidget {
+  static String routeName = '/user';
+  UserView({super.key});
+
+  @override
+  State<UserView> createState() => UserViewState();
+}
+
+class UserViewState extends State<UserView> {
+  int index = 0;
+  final screens = [
+    HomePage(),
+    HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Dashboard',
+          ),
+        ),
+        backgroundColor: const Color(0xFF6c1915),
+        automaticallyImplyLeading: false,
+      ),
+      body: PageView(
+        children: screens,
+        onPageChanged: (index) => setState(() => this.index = index),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text(
+          'Add Entry',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        icon: const Icon(Icons.add_outlined),
+        onPressed: () {},
+        backgroundColor: const Color(0xFF944446),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFe8e8e8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: const Offset(0, -3),
+            ),
+          ],
+        ),
+        child: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: const Color(0xFF944446),
+            labelTextStyle: MaterialStateProperty.all(
+              const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          child: NavigationBar(
+            height: 60,
+            backgroundColor: const Color(0xFFf8fbf8),
+            selectedIndex: index,
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            animationDuration: const Duration(milliseconds: 1000),
+            onDestinationSelected: (index) =>
+                setState(() => this.index = index),
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home_outlined),
+                selectedIcon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.person_outlined),
+                selectedIcon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
