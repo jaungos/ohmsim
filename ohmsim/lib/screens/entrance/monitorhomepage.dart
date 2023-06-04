a01d07ae3550103df0b452c102b64653d23b7b72
jlaungos <jaungos@up.edu.ph>
2023-06-05 00:12:01 +0800

    Feat: added the widget for the list of student logs who entered the location

--- ohmsim/lib/screens/entrance/monitorhomepage.dart
+++ ohmsim/lib/screens/entrance/monitorhomepage.dart
@@ -19,6 +19,7 @@ class MonitorHomePageState extends State<MonitorHomePage> {
       child: Column(
         children: [
           monitorHeader(),
+          studentLogsHeader(),
         ],
       ),
     );
@@ -60,6 +61,74 @@ class MonitorHomePageState extends State<MonitorHomePage> {
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  // Widget for the student logs header
+  Widget studentLogsHeader() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 5, 15, 0),
+      child: Column(
+        children: [
+          Row(
+            children: const [
+              Text(
+                'Students Logs',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF191313),
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Widget for the list of student logs
+  Widget studentLogs() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: const Color(0xFFffebc7),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: Column(
+                children: [
+                  ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: sample['requests'].length,
+                    separatorBuilder: (context, index) {
+                      return const Divider(
+                        thickness: 2,
+                      );
+                    },
+                    itemBuilder: ((context, index) {
+                      return Center(
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          // @TODO: change the layout gayahin medj 'yung sa google classroom
+                          child: ListTile(
+                            dense: true,
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
